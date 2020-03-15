36d2e1c0427b88dbcbeebd74f712e45c379817fe
MBenincasa <m.benincasa13@studenti.unisa.it>
2020-03-15 19:23:24 +0100

    Modifiche dati

--- src/app/pages/display-data/display-data.page.ts
+++ src/app/pages/display-data/display-data.page.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
-  selector: 'app-display-data',
-  templateUrl: './display-data.page.html',
-  styleUrls: ['./display-data.page.scss'],
+  selector: "app-display-data",
+  templateUrl: "./display-data.page.html",
+  styleUrls: ["./display-data.page.scss"]
 })
 export class DisplayDataPage implements OnInit {
+  data: any;
+  dispRegioni: boolean;
 
-  data: any[];
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private dataService: DataService) {
+    this.dispRegioni = false;
   }
 
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const idRegione = +params.get("idRegione");
+      const nomeRegione = params.get("nomeRegione");
+
+      if (idRegione && nomeRegione) {
+        this.dispRegioni = true;
+        this.data = this.dataService.getMostRecentRegionalDataFor({code: idRegione, name: nomeRegione})
+      } else {
+        this.data = this.dataService.getMostRecentNationalData();
+      }
+    });
+  }
 }
