d06fa51098ac0aa61e4eb0f85a4f0107aedff9fa
MBenincasa <m.benincasa13@studenti.unisa.it>
2020-03-18 19:58:36 +0100

    Grafici

--- src/app/pages/display-data/display-data.page.ts
+++ src/app/pages/display-data/display-data.page.ts
@@ -4,6 +4,8 @@ import { DataService } from "src/app/services/data.service";
 import { Observable } from "rxjs";
 import { Chart, ChartDataSets } from "chart.js";
 import { Label, Color } from "ng2-charts";
+import { DatePipe } from "@angular/common";
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: "app-display-data",
@@ -11,22 +13,28 @@ import { Label, Color } from "ng2-charts";
   styleUrls: ["./display-data.page.scss"]
 })
 export class DisplayDataPage implements OnInit {
+
+  @ViewChild(BaseChartDirective, {static: false}) chart: BaseChartDirective;
+
   chartData: ChartDataSets[] = [{ data: [], label: "COVID-19" }];
   chartLabels: Label[];
   chartOptions = {
     responsive: true
   };
-  chartType = "doughnut";
 
+  chartType;
   data: any;
+  historicalData: any[];
   dispRegioni: boolean;
   idRegione;
   nomeRegione;
   provincesData: any[];
   chartColors: Color[];
+  private datePipe: DatePipe;
 
   constructor(private route: ActivatedRoute, private dataService: DataService) {
     this.dispRegioni = false;
+    this.datePipe = new DatePipe("IT");
   }
 
   ngOnInit() {
@@ -38,14 +46,16 @@ export class DisplayDataPage implements OnInit {
         this.dispRegioni = true;
         const region = { code: this.idRegione, name: this.nomeRegione };
         this.dataService
-          .getMostRecentRegionalDataFor(region)
-          .subscribe(async regionalData => {
-            this.data = regionalData;
+          .getHistoricalDataFor(region)
+          .subscribe(regionalData => {
+            this.data = regionalData[0];
+            this.historicalData = regionalData;
             this.initChart();
           });
       } else {
-        this.dataService.getMostRecentNationalData().subscribe(nationalData => {
-          this.data = nationalData;
+        this.dataService.getNationalData().subscribe(nationalData => {
+          this.data = nationalData[0];
+          this.historicalData = nationalData;
           this.initChart();
         });
       }
@@ -55,6 +65,7 @@ export class DisplayDataPage implements OnInit {
   private initChart() {
     this.chartLabels = [];
     this.chartData[0].data = [];
+    this.chartType = "bar";
 
     this.chartLabels.push("Contagiati", "Deceduti", "Guariti");
     this.chartData[0].data.push(
@@ -69,13 +80,22 @@ export class DisplayDataPage implements OnInit {
     ];
   }
 
-  generateColorArray(num) {
-    this.chartColors = [];
-    for (let i = 0; i < num; i++) {
-      this.chartColors.push({
-        borderColor: "#000000",
-        backgroundColor: "#" + Math.floor(Math.random() * 16777215).toString(16)
-      });
+  private initChartAndamento() {
+    this.chartLabels = [];
+    this.chartData[0].data = [];
+    this.chartType = "line";
+
+    for (let item of this.historicalData) {
+      this.chartLabels.push(this.datePipe.transform(item.data, "dd/MM/yyyy"));
+      this.chartData[0].data.push(item.totale_casi);
     }
+
+    this.chartColors = [{backgroundColor: "#445566"}]
+
+    this.chart.chart.update();
+  }
+
+  onClick() {
+    this.initChartAndamento();
   }
 }
